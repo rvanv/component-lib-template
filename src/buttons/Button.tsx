324998dd5ee87f67821b71421c36ffb94074813b
Ryan Van Valkenburg <674162@usgvlrw2tqnc.babgsetc.pvt>
2021-12-22 21:08:20 -0800

    starting button api

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export const Button: React.FC = () => {
-  return <button>Hello World</button>;
-};
+export const Button = React.forwardRef<HTMLButtonElement, React.ComponentPropsWithoutRef<'button'>>((
+  { children, ...props }, ref
+) => {
+  return <button ref={ref} type="button" {...props}>{children}</button>;
+});
+
+Button.displayName = "Button";
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -7,6 +7,6 @@ export default {
   component: Button,
 } as Meta;
 
-const Template: Story = (args) => <Button {...args} />;
+const Template: Story = (args) => <Button {...args}>My Button Component</Button>;
 
 export const Default = Template.bind({});
